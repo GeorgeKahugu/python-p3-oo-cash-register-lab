8e8d3166a2300f825655538090bf840c99f3c4eb
GeorgeKahugu <gee.kahugu@gmail.com>
2024-05-25 18:18:49 +0300

    python cash register lab

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,41 +1,4 @@
 #!/usr/bin/env python3
-
-# class CashRegister:
-#   def __init__(self, discount=0):
-#     self.total=0
-#     self.discount = discount
-#     self.items = []
-#   pass
-
-
-# class CashRegister:
-#     def __init__(self, discount=0):
-#         self.total = 0
-#         self.discount = discount
-#         self.items = []
-
-#     def add_item(self, title, price, quantity=1):
-#         for _ in range(quantity):
-#             self.items.append(title)
-#             self.total += price
-
-#     def apply_discount(self):
-#         if self.discount > 0:
-#             self.total -= (self.total * self.discount) / 100
-#             print(f"After the discount, the total comes to ${self.total:.2f}.")
-#         else:
-#             print("There is no discount to apply.")
-
-#     def void_last_transaction(self):
-#         if self.items:
-#             last_price = self.total
-#             last_item = self.items.pop()
-#             self.total -= last_price
-#             print(f"Removed {last_item} from the transaction. New total is ${self.total:.2f}.")
-#         else:
-#             print("There are no transactions to void.")
-
-
 class CashRegister:
     def __init__(self, discount=0):
         self.total = 0
